40f45bc656471333e772524a919dd1ac0e9d1fd0
0gravity <0gravity000@gmail.com>
2021-08-23 12:35:35 +0900

    Add Post model,migration,controller

---
+++ database/migrations/2021_08_23_122729_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('language_id');
+            $table->bigInteger('tag_id');
+            $table->text('code');
+            $table->text('note1')->nullable();
+            $table->text('note2')->nullable();
+            $table->text('note3')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
